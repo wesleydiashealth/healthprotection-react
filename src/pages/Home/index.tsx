68b72831c2a3edbcad9a3e106721df970f07b3a9
Guilherme Guain <guilherme.guain@gmail.com>
2022-02-23 11:45:33 -0300

    region test

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,8 +11,6 @@ import Habits from 'components/Habits';
 
 import { AppProvider } from 'contexts/app';
 
-import GetSubdomain from 'services/GetSubdomain';
-
 import Container from './styles';
 
 const Home: React.FC = () => {
@@ -24,27 +22,8 @@ const Home: React.FC = () => {
   // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
   // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
 
-  const { hostname } = window.location;
-
-  const parts = hostname.split('.');
-
-  let lastIndex = -2;
-  const last = parts[parts.length - 1];
-  const isLocalhost = last === 'localhost';
-  if (isLocalhost) {
-    lastIndex = -1;
-  }
-
-  const subdomain = parts.slice(0, lastIndex).join('.').replace('.react', '');
-
   return (
     <Container>
-      <p>Hostname: {hostname}</p>
-      <p>Last: {last}</p>
-      <p>LastIndex: {lastIndex}</p>
-      <p>Subdomain 1: {parts.slice(0, lastIndex).join('.')}</p>
-      <p>Subdomain 2: {subdomain}</p>
-      <p>Subdomain 3: {GetSubdomain()}</p>
       <AppProvider>
         {/* <Hero /> */}
         <Wizard />
