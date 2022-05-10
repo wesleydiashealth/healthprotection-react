dc307bc22b009939ea25b6e5f5e940bd91aaaa43
Guilherme Guain <guilherme.guain@gmail.com>
2022-05-10 14:28:10 -0300

    updated amazon tags

--- src/services/GetAmazonTag.ts
+++ src/services/GetAmazonTag.ts
@@ -1,12 +1,12 @@
 export default function GetAmazonTag(region = 'com'): string {
   switch (region) {
     case 'com':
-      return 'healthprote09-20';
+      return 'healthprote05-20';
 
     case 'es':
       return 'healthprote04-21';
 
     default:
-      return 'healthprote09-20';
+      return 'healthprote05-20';
   }
 }
